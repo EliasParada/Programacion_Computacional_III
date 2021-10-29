e391193630727af32d0b8447d03ca2de8bf4dfc4
vlainezt <vlainezt07@gmail.com>
2021-10-29 10:35:03 -0600

    a

--- server.py
+++ server.py
@@ -1,21 +1,21 @@
-#Conectar mysql con python
-import pymysql
+# #Conectar mysql con python
+# import pymysql
 
-#Conectar con la base de datos
-conn = pymysql.connect(host='localhost',
-                       user='root',
-                       password='',
-                       db='sistema',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
+# #Conectar con la base de datos
+# conn = pymysql.connect(host='localhost',
+#                        user='root',
+#                        password='',
+#                        db='sistema',
+#                        charset='utf8mb4',
+#                        cursorclass=pymysql.cursors.DictCursor)
 
-#Crear un cursor
-cursor = conn.cursor()
+# #Crear un cursor
+# cursor = conn.cursor()
 
-#Crear una funcion para obtener el id de un usuario
-def get_user_id(username):
-    #Obtener el id del usuario
-    cursor.execute("SELECT id FROM usuarios WHERE username = '{}'".format(username))
-    return cursor.fetchone()
+# #Crear una funcion para obtener el id de un usuario
+# def get_user_id(username):
+#     #Obtener el id del usuario
+#     cursor.execute("SELECT id FROM usuarios WHERE username = '{}'".format(username))
+#     return cursor.fetchone()
 
-print(get_user_id('admin'))
+# print(get_user_id('admin'))
