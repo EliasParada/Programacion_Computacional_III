1954458dac28901566e03dad916f499433be26b4
eliasparada <eliasparadalozano@gmail.com>
2021-10-28 23:06:53 -0600

    AGregando una conexión a mysql generada por copilot

--- server.py
+++ server.py
@@ -1,36 +1,21 @@
-import datetime
+#Conectar mysql con python
+import pymysql
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
+#Conectar con la base de datos
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',
+                       db='sistema',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
-def daybetween(date1, date2):
-    """
-    Returns the number of days between two dates.
-    """
-    return (date2 - date1).days
+#Crear un cursor
+cursor = conn.cursor()
 
-def yearbetween(date1, date2):
-    """
-    Returns the number of years between two dates.
-    """
-    return (date2.year - date1.year)
+#Crear una funcion para obtener el id de un usuario
+def get_user_id(username):
+    #Obtener el id del usuario
+    cursor.execute("SELECT id FROM usuarios WHERE username = '{}'".format(username))
+    return cursor.fetchone()
 
-date1 = input("Enter the first date (YYYY-MM-DD): ")
-date1 = datetime.datetime.strptime(date1, "%Y-%m-%d")
-date2 = input("Enter the second date (YYYY-MM-DD): ")
-date2 = datetime.datetime.strptime(date2, "%Y-%m-%d")
-
-print("The number of days between the two dates is:", daybetween(date1, date2))
-print("The number of years between the two dates is:", yearbetween(date1, date2))
-print("The number of months between the two dates is:", yearbetween(date1, date2) * 12 + daybetween(date1, date2) // 30)
-
-class basicServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"<html><body><h1>Hello World</h1></body></html>")
-
-    def do_POST(self):
-        data = self.rfile.read(int(self.headers['Content-Length']))
-        self.send_response(200)
+print(get_user_id('admin'))
