b71394d6c66aa9332edfbe6b8772cf14718559c4
eliasparada <eliasparadalozano@gmail.com>
2021-11-15 21:36:05 -0600

    Eliminamos archivos innecesarios, agregamos una archivo separado para la ia, y exportamos una ai entrenada (actualmente con fashion_mnist para pruebas)

---
+++ model.py
@@ -0,0 +1,72 @@
+# Import tensorflow, and tensorflow_datasets
+import math
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Import the Fashion MNIST dataset
+dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+trainset, testset = dataset["train"], dataset["test"]
+
+# Creta the list of the labels
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Add the data to the train and test sets
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
+print("Number of training examples: {}".format(num_train_examples))
+print("Number of test examples:     {}".format(num_test_examples))
+
+# Define the function to preprocess the data
+def normalize(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+# Create the train and test datasets
+train_dataset = trainset.map(normalize)
+test_dataset = testset.map(normalize)
+
+# Create the model
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+#     tf.keras.layers.MaxPooling2D(2,2),
+#     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+#     tf.keras.layers.MaxPooling2D(2,2),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(128, activation='relu'),
+#     tf.keras.layers.Dense(10, activation='softmax')
+# ])
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Create the batch size
+batch_size = 32
+train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(batch_size)
+test_dataset = test_dataset.repeat().shuffle(num_test_examples).batch(batch_size)
+
+# Fit the model
+model.fit(train_dataset, epochs=10, steps_per_epoch=math.ceil(num_train_examples/batch_size))
+
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/batch_size))
+print('Lost: ', test_loss)
+print('Accuracy on test dataset:', test_accuracy)
+
+# Export the model
+model.save('fsmodel.h5')
+
+# Test the model
+for test_images, test_labels in test_dataset.take(1):
+    test_images = test_images.numpy()
+    test_labels = test_labels.numpy()
+    predictions = model.predict(test_images)
+    print(predictions[0])
+    print(test_labels[0])
--- server.py
+++ server.py
@@ -3,69 +3,17 @@ import numpy as np
 import mysql.connector
 import urllib.request
 import json
-import math
 
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 from http import cookies
+import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_datasets as tfds
 
-#Cargar el dataset de imagenes
-dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
-datos_entrenamiento, datos_prueba = dataset["train"], dataset["test"]
-#Crear las etiquetas de los productos
-tags = ["Camiseta", "Pantalón", "Sueter", "Vestido", "Abrigo", "Sandalia", "Camisa", "Zapatilla", "Bolso", "Bota"]
+# CARGAR EL MODELO
+model = tf.keras.models.load_model('fsmodel.h5')
 
-#pasar en variables los datos de entrenamiento y prueba
-numeros_ejemplo_entrenamiento = metadata.splits["train"].num_examples
-numeros_ejemplo_prueba = metadata.splits["test"].num_examples
-print(numeros_ejemplo_entrenamiento, numeros_ejemplo_prueba)
-
-#funcion normalizadora de los pixeles de 0 - 255 que sean de 0 - 1
-def normalizacion(images, labels):
-  images = tf.cast(images, tf.float32)
-  images /= 255 # 150/255=0.588
-  return images, labels
-
-#llamamoas la funcion normalizadora en ambos dataset
-datos_entrenamiento = datos_entrenamiento.map(normalizacion)
-datos_prueba = datos_prueba.map(normalizacion)
-
-#estruturamos nuestra red neuronal
-modelo = tf.keras.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28,28,1)), #capa de entreda de 28*28 = 784 neuronas una para cada pixel
-        tf.keras.layers.Dense(64, activation=tf.nn.relu), #capa oculta con 64 neuronas, funcion de activacion relu
-        tf.keras.layers.Dense(64, activation=tf.nn.relu), #capa oculta con 64 neuronas, funcion de activacion relu
-        tf.keras.layers.Dense(10, activation=tf.nn.softmax) #capa de salida con 10 neuronas una para cada numero, 
-        #funcion de activacion softmax. esta funcion es requerida para clasificacion
-])
-
-#Compilamos el modelo de red neuronal e indicamos las funciones a utilizar 
-modelo.compile(
-    optimizer="adam",
-    loss="sparse_categorical_crossentropy",
-    metrics=['accuracy']
-)
-
-#Entrenamiento por lotes
-batchsize = 32
-datos_entrenamiento = datos_entrenamiento.repeat().shuffle(numeros_ejemplo_entrenamiento).batch(batchsize)
-datos_prueba = datos_prueba.repeat().shuffle(numeros_ejemplo_prueba).batch(batchsize)
-
-#entrenado nuetro modelo de datos
-modelo.fit(datos_entrenamiento, epochs=5, 
-           steps_per_epoch=math.ceil(numeros_ejemplo_entrenamiento/batchsize))
-
-#evaluar el modelo con los datos de prueba
-test_loss, test_accuracy = modelo.evaluate(datos_prueba, steps=math.ceil(numeros_ejemplo_prueba/batchsize))
-print("Resultado de las pruebas", test_loss, test_accuracy)
-
-# Predecir 1 imagen del dataset de prueba
-for imagen, etiqueta in datos_prueba.take(1):
-    prediccion_probabilidad = modelo.predict(imagen)
-    prediccion = tf.math.argmax(prediccion_probabilidad, axis=1)
-    print("Etiqueta de la imagen: ", tags[etiqueta[0]])
-    print("Prediccion: ", tags[prediccion.numpy()[0]])
+# CREAMOS LA LISTA DE LAS ETIQUETAS
+tags = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 #######CREAMOS UNA CLASE PARA DMINISTRAR LAS SESIONES DE LA APLICACION CON COOKIES#######
 class SessionManager:
@@ -279,7 +227,8 @@ class localServer(SimpleHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
-        data = parse.unquote(body.decode('utf-8'))
+        data = body.decode()
+        data = parse.unquote(data)
         print(data)
 
         if self.path == '/admin_category':
@@ -307,15 +256,24 @@ class localServer(SimpleHTTPRequestHandler):
             self.wfile.write(json.dumps(dict(response=response)).encode('utf-8'))
 
         if self.path == '/testai':
+            print(data)
             matriz = np.fromstring(data, np.float32, sep=',')
             matriz = matriz.reshape(28,28)
             matriz = np.array(matriz)
             matriz = matriz.reshape(1,28,28,1)
+            print(matriz.shape)
 
-            prediccion = modelo.predict(matriz,batch_size=1)
+            plt.figure()
+            plt.imshow(matriz[0,...], cmap=plt.cm.binary)
+            plt.colorbar()
+            plt.grid(False)
+            plt.show()
+            
+            prediccion = model.predict(matriz,batch_size=1)
             prediccion = str(np.argmax(prediccion))
             prediccion = tags[int(prediccion)]
             print(prediccion)
+
             self.send_response(200)
             self.send_header("Access-Control-Allow-Origin","*")
             self.end_headers()
